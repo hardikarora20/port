7e305d6ab549aa34eeaffce9caff320d508d313e
hardikkk20 <76209465+hardikkk20@users.noreply.github.com>
2022-04-22 20:07:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 function nav(){
-    // document.getElementById("cross").style.backgroundColor="Pink";
-    // document.getElementsByTagName('ul')[0].style.right="1%";
-    // document.getElementsByTagName('ul')[0].style.transform="scaleX(0)";
-    if(document.getElementsByTagName('ul')[0].style.display=="none"){
-    document.getElementById("cross").style.transform="rotate(180deg)";
-    document.getElementsByTagName('ul')[0].style.display="flex";
-    //document.getElementsByTagName('ul')[0].style.marginLeft="100px";
+    if(document.getElementsByTagName('ul')[0].style.transform=="translateX(0%)"){
+        document.getElementById("cross").style.transform="rotate(0deg)";
+        document.getElementsByTagName('ul')[0].style.transform="translateX(98%)";
+        document.getElementById("cross").style.right="-1%";
     }
     else{
-    document.getElementById("cross").style.transform="rotate(0deg)";
-    document.getElementsByTagName('ul')[0].style.display="none";
+        document.getElementById("cross").style.transform="rotate(180deg)";
+        document.getElementsByTagName('ul')[0].style.transform="translateX(0%)";
+        document.getElementById("cross").style.right="1.65%";
     }
 }
+
